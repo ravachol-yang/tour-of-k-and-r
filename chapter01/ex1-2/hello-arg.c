1e85212f52bc255972fbe4dc772774854a21dbb0
Ravachol <ravachol.yang@gmail.com>
2024-02-26 08:45:43 +0800

    Ex 1-2

---
+++ chapter01/ex1-2/hello-arg.c
@@ -0,0 +1,10 @@
+/*
+Exercise 1-2. Experiment to find out what happens when prints's argument string contains
+\c, where c is some character not listed above.
+*/
+#include <stdio.h>
+
+main()
+{
+  printf("hello\c");
+}
