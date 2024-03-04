17d28ac15d2f9129aadb8126d70b073ab625dfec
Ravachol <ravachol.yang@gmail.com>
2024-03-04 23:06:43 +0800

    ex1-15

---
+++ chapter01/ex1-15/convert-function.c
@@ -0,0 +1,31 @@
+/*
+  Exercise 1.15. Rewrite the temperature conversion program of Section 1.2 to use a function
+  for conversion.
+ */ 
+#include <stdio.h>
+
+float convert(float fahr);
+
+main ()
+{
+  float fahr, celsius;
+  float lower, upper, step;
+
+  lower = 0; /* the lower bound of fahr temp*/
+  upper = 300; /* the upper bound of fahr temp */
+  step = 20; /* increasement step */
+
+  fahr = lower;
+
+  printf("FAHR\tCELS\n"); /* add header */
+  
+  while (fahr <= upper) {
+    printf("%6.0f\t%3.1f\n",fahr,convert(fahr));
+    fahr = fahr + step;
+  }
+}
+
+float convert(float fahr)
+{
+  return (5.0/9.0) * (fahr -32.0);
+}
