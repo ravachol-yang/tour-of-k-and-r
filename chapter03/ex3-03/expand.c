c4143584e451bcd34c87744a6105da0152c202de
Ravachol <ravachol.yang@gmail.com>
2024-06-25 14:00:08 +0800

    fucked up ,bad exercise

--- chapter03/ex3-03/expand.c
+++ chapter03/ex3-03/expand.c
@@ -6,7 +6,45 @@
  */
 #include <stdio.h>
 
+#define MAX 1000
+
+void expand(char s1[], char s2[]);
+
 int main ()
 {
+    char s1[] = "asdasda-h1212a-haaa";
+    char s2[MAX];
+
+    expand(s1, s2);
+
+    printf(s2);
+    printf("\n");
+
     return 0;
+}
+
+void expand(char s1[], char s2[])
+{
+    int i,j = 0;
+    int k;
+
+    for (i=0; s1[i] != '\0'; ++i) {
+	k = 1;
+	if (s1[i] == '-') {
+	    if ((s1[i-1] >= 'a' && s1[i+1] <= 'z') || (s1[i-1] >= '0' && s1[i+1] <= '9')) {
+		if (s1[i-1] < s1[i+1]) {
+		    while (s2[j] <= s1[i+1]) {
+			s2[j] = s1[i-1]+k;
+			++j;
+			++k;
+		    }
+		    ++i;
+		    k = 0;
+		}
+	    }
+	}
+	s2[j] = s1[i];
+	++j;
+    }
+    s2[j] = '\0';
 }
